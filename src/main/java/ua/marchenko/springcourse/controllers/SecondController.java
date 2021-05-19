dad664cb546cf8bbb189a18daaf1c18adfe9d83c
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-05-19 20:06:37 +0300

    Add simple controllers and views

---
+++ src/main/java/ua/marchenko/springcourse/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package ua.marchenko.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+}
