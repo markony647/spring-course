fa4f1ef2b27d31f27e7f69306248626f78cf46fc
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-05-25 12:30:24 +0300

    Add validation for forms

--- src/main/java/ua/marchenko/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/marchenko/springcourse/dao/PersonDAO.java
@@ -13,11 +13,11 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Sam"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 45, "bob@yahho.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Sam", 18, "sam@sfd.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 87, "dsfds@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 33, "katy@perry.com"));
     }
 
     public List<Person> index() {
@@ -36,6 +36,8 @@ public class PersonDAO {
     public void update(int id, Person updatedPerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
