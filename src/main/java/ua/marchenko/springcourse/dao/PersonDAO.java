ac40203bc2ea83bdaea3ae7fa05d8c799c19fdf9
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-05-29 13:35:58 +0300

    Add low level Postgres JDBC realization of index() method

--- src/main/java/ua/marchenko/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/marchenko/springcourse/dao/PersonDAO.java
@@ -3,12 +3,19 @@ package ua.marchenko.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ua.marchenko.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USER_NAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection;
     private List<Person> people;
 
     {
@@ -20,7 +27,36 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Katy", 33, "katy@perry.com"));
     }
 
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        String query = "SELECT * FROM Person";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
